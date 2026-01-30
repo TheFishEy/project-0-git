943a67f27d9a48794b8115d486d55b55697105d3
Patrick Hung <78626322+TheFishEy@users.noreply.github.com>
2026-01-30 12:33:02 -0800

    3rd iteration showing program in java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,30 @@
 //package project-0-git;
 
 public class Hello {
-    public static void main(String[]args){
-        System.out.println("Hello world");
+    public static void main(String[]args)
+    {
+        System.out.println("Program to compute sum");
+
+        int n =  10;
+        int sum = computeSum(n);
+        System.out.println("sum = " + sum);
+
+        //int result = add(n, sum);
+        //System.out.println("result = " + result);
+
+    }
+
+    static int computeSum(int n) 
+    {
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum = add(sum, i);
+        }
+        return sum;
     }
     
+    static int add(int a, int b) {
+        int result = a + b;
+        return result;
+    }
 }
