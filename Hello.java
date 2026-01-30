91b60cec0416025ac0b00f42cb77c4acc42dbe3c
Patrick Hung <78626322+TheFishEy@users.noreply.github.com>
2026-01-30 12:41:12 -0800

    Revert "breaking program in 4th iteration"
    This reverts commit 61bdb24f204a1b8bb1ad63d1c22973e92da536da.

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
 public class Hello {
     public static void main(String[]args)
     {
-        System.out.println("Program to compute sum")
+        System.out.println("Program to compute sum");
 
         int n =  10;
         int sum = computeSum(n);
